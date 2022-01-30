66f71aa53fddea09ee9d8fd9c1b5ab5a2800b0d8
Alyson <ramos.alysonfarias@gmail.com>
2022-01-30 01:10:44 -0300

    build on netlify working, but got problems in vercel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { BsLightbulb, BsLightbulbOff } from 'react-icons/bs';
+import { BsLightbulb, BsLightbulbOff } from 'react-icons/bs';
 
 
 const Header = ({handleToggleDarkMode, darkMode}) => {
@@ -7,14 +7,14 @@ const Header = ({handleToggleDarkMode, darkMode}) => {
         <div className='header'>
             <h1>NotNote</h1>
             <a href='https://github.com/alysonfarias' class="easter-egg">You found this, but don't click here :D</a>
-            {/* {darkMode && <BsLightbulb visibility={true} class="btn" onClick={()=> 
+            {darkMode && <BsLightbulb visibility={true} class="btn" onClick={()=> 
                 handleToggleDarkMode(
                     (previusDarkMode)=> !previusDarkMode )
                 }/> }
             {!darkMode &&<BsLightbulbOff class="btn" onClick={()=> 
                 handleToggleDarkMode(
                     (previusDarkMode)=> !previusDarkMode )
-                }/>} */}
+                }/>}
         </div>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import {MdSearch} from 'react-icons/md'
+import {MdSearch} from 'react-icons/md'
 
 
 
 const Search = ({handleSearchNote}) => {
 return(
     <div className='search'>
-        {/* <MdSearch className="search-icon" size='1.3em'></MdSearch> */}
+        <MdSearch className="search-icon" size='1.3em'></MdSearch>
         <input type='text'
          onChange={ (event) => 
             handleSearchNote(event.target.value)}
